d575c6d2b39b0bf4a470c12563b42a3d7b477602
VJWQ <wenqi.jia@gatech.edu>
2022-11-23 18:22:16 -0500

    update page

--- slowfast/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-
-from .build import MODEL_REGISTRY, build_model
-from .build import G_REGISTRY, build_generator
-from .build import D_REGISTRY, build_discriminator
-from .build import DE_REGISTRY, build_decoder
-from .build import EN_REGISTRY, build_encoder
-from .g_d_net import generator, discriminator
